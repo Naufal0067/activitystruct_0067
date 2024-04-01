f3d34e2a531ae286216f8f51928e2413d2bb1457
Naufal0067 <naufalopang1928@gmail.com>
2024-04-01 15:45:21 +0700

    memasukkan int main

--- strukturC_3.cpp
+++ strukturC_3.cpp
@@ -15,5 +15,27 @@ struct Mahasiswa{
 };
 
 int main(){
-    
+    Mahasiswa mhs;
+
+    cout << "Nomor Mahasiswa;
+    cin >> mhs.nim;
+    cout <<"Nama Mahasiswa :";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa "<<endl;
+    cout << "it Nama Desa: ";
+    cin >> mhs,alamat.desa;
+    cout << "\t Nama Kota
+    cin>> mhs.alamat.kota;
+
+    cout << "Unur Mahasiswa: ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout <<"\n NIM: "<<mhs.nim;
+    cout << "\n Nama: "<< mhs.nama;
+    cout << "in Alamat:";
+    cout << "\n \t Desa: "<< mhs.alamat.desa;
+    cout << "\n \t Kota: "<< mhs.alamat.kota;
+    cout << "in mur: "<< nhs.umur;
+}
 }
