40b0ecc00fa816a8b435ae273d912455f976e27e
Naufal0067 <naufalopang1928@gmail.com>
2024-04-01 15:29:13 +0700

    menambahkan struct detail alamat dan mahasiswa

---
+++ struct_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct DetaliAlamat{
+    string desa;
+    string kota;
+
+};
+
+struct Mahasiswa{
+    string nim;
+    string nama;
+    DetaliAlamat alamat;
+    int umur;
+};
