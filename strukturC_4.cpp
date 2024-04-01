f8f3212b7e4036bbd2899821b8101ccad750c7dd
Naufal0067 <naufalopang1928@gmail.com>
2024-04-01 16:07:30 +0700

    menambahkan cout data ke

--- strukturC_4.cpp
+++ strukturC_4.cpp
@@ -19,30 +19,32 @@ struct Mahasiswa{
 int main(){
     Mahasiswa mhs[3];
     for (int i=0;i<3;i++){
+        cout <<"Data ke-"<<(i+1)<<":"<<endl;
         cout << "Nomor Mahasiswa : ";
-    getline(cin , mhs.nim);
-    cout <<"Nama Mahasiswa : ";
-    getline(cin , mhs.nama);
+        getline(cin , mhs.nim);
+        cout <<"Nama Mahasiswa : ";
+        getline(cin , mhs.nama);
 
 
 
-    cin >> mhs.nama;
-    cout << "Alamat Mahasiswa "<<endl;
-    cout << "it Nama Desa: ";
-    cin >> mhs,alamat.desa;
-    cout << "\t Nama Kota
-    cin>> mhs.alamat.kota;
+        cin >> mhs.nama;
+        cout << "Alamat Mahasiswa "<<endl;
+        cout << "it Nama Desa: ";
+        cin >> mhs,alamat.desa;
+        cout << "\t Nama Kota
+        cin>> mhs.alamat.kota;
 
-    cout << "Unur Mahasiswa: ";
-    cin >> mhs.umur;
+        cout << "Unur Mahasiswa: ";
+        cin >> mhs.umur;
     }
     
-
-    cout << endl;
-    cout <<"\n NIM: "<<mhs.nim;
-    cout << "\n Nama: "<< mhs.nama;
-    cout << "in Alamat:";
-    cout << "\n \t Desa: "<< mhs.alamat.desa;
-    cout << "\n \t Kota: "<< mhs.alamat.kota;
-    cout << "in umur:"<<mhs.umur;
-}
+    for (int i=0;i<3;i++){
+        cout << endl;
+        cout <<"\n NIM: "<<mhs.nim;
+        cout << "\n Nama: "<< mhs.nama;
+        cout << "in Alamat:";
+        cout << "\n \t Desa: "<< mhs.alamat.desa;
+        cout << "\n \t Kota: "<< mhs.alamat.kota;
+        cout << "in umur:"<<mhs.umur;
+    }
+ }   
